c98efa6f960f6c6df70cbd8642649a171cc98cfd
Programmer00777 <55809302+Programmer00777@users.noreply.github.com>
2022-10-04 13:59:08 +0300

    Checkpoint-commit: fixing the issue with mongoose.connect()

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+/* App Configuration */
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json);
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
+});
+
+/* MongoDB Connection */
+mongoose.connect(process.env.MONGO_URI, {useUnifiedTopology: true, useNewUrlParser: true});
+
+/* Creating Schemas */
+const { Schema } = mongoose;
+
+const ExerciseSchema = new Schema({
+    userId: {type: String, required: true},
+    description: String,
+    duration: Number,
+    date: Date
+});
+
+const UserSchema = new Schema({
+    username: String
+});
+
+const User = mongoose.model('User', UserSchema);
+const Exercise = mongoose.model('Exercise', ExerciseSchema);
+
+/* Listener Configuration */
+const listener = app.listen(process.env.PORT || 3000, () => {
+    console.log("The app is listening on port " + listener.address().port);
+});
